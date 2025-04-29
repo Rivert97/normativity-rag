06303856aed585f1225ed2128db0cbc3950cdcea
Roberto Garcia <betogarcia97@live.com.mx>
2025-04-29 13:58:00 -0600

    New option 'csv' added to print or save csv data to a file

--- cli.py
+++ cli.py
@@ -21,11 +21,14 @@ class CLIController():
     """
     def __init__(self):
         self._logger = AppLogger.get_logger('CLIController')
+
+        self.print_to_console = True
+
         self._args = self.__process_args()
 
     def run(self):
         if self._args.file != '':
-            self.__process_file()
+            self.__process_file(self._args.file, self._args.output)
         elif self._args.directory != '':
             self.__process_directory()
         else:
@@ -41,8 +44,9 @@ class CLIController():
         parser.add_argument('--cache-dir', default='./.cache/', type=str, help='Directory to be used as cache. Defaults to ./.cache/')
         parser.add_argument('-d', '--directory', default='', type=str, help='Directory to be processed in directory mode')
         parser.add_argument('-f', '--file', default='', type=str, help='File to be processed in single file mode')
-        parser.add_argument('-o', '--output', default='./', type=str, help='File or directory to store the output text file(s). When -d is used, this defaults to ./')
+        parser.add_argument('-o', '--output', default='', type=str, help='File or directory to store the output text file(s). When -d is used, this defaults to ./')
         parser.add_argument('-p', '--page', type=int, help='Number of page to be processed')
+        parser.add_argument('-t', '--type', default='txt', choices=['txt', 'csv'], type=str, help='Type of output')
         parser.add_argument('--version', action='store_true', help='Show version of this tool')
 
         args = parser.parse_args()
@@ -60,6 +64,8 @@ class CLIController():
             raise CLIException("Parent cache directory must exist")
 
         if args.directory != '':
+            if args.output == '':
+                args.output = './'
             if not os.path.exists(args.output):
                 raise CLIException("Destination directory does not exist")
 
@@ -68,33 +74,52 @@ class CLIController():
             if dirname != '' and not os.path.exists(dirname):
                 raise CLIException("Output path does not exist")
 
+        if args.output != '':
+            self.print_to_console = False
+
         return args
 
-    def __process_file(self):
-        pdf_loader = PdfMixedLoader(self._args.file, self._args.cache_dir)
+    def __process_file(self, filename: str, output: str = None):
+        pdf_loader = PdfMixedLoader(filename, self._args.cache_dir)
         if self._args.page != None:
-            text = pdf_loader.get_page_text(self._args.page)
+            pdf_loader.process_page(self._args.page)
         else:
-            text = pdf_loader.get_text()
+            pdf_loader.process_document()
 
-        if self._args.output == './':
-            print(text)
-        else:
-            self.__save_text(text, self._args.output)
+        self.__make_output(pdf_loader, output)
 
     def __process_directory(self):
         for file in glob.glob(f'{self._args.directory}/*.pdf'):
             basename = ''.join(os.path.basename(file).split('.')[:-1])
-
-            pdf_loader = PdfMixedLoader(file, self._args.cache_dir)
             if self._args.page != None:
-                text = pdf_loader.get_page_text(self._args.page)
                 out_name = f"{self._args.output}/{basename}_{self._args.page}.txt"
             else:
-                text = pdf_loader.get_text()
                 out_name = f"{self._args.output}/{basename}.txt"
 
-            self.__save_text(text, out_name)
+            self.__process_file(file, out_name)
+
+    def __make_output(self, pdf_loader: PdfMixedLoader, filename: str = None):
+        if self._args.type == 'txt':
+            if self._args.page is not None:
+                text = pdf_loader.get_page_text(self._args.page)
+            else:
+                text = pdf_loader.get_text()
+
+            if self.print_to_console:
+                print(text)
+            else:
+                self.__save_text(text, filename)
+        elif self._args.type == 'csv':
+            if self.print_to_console:
+                if self._args.page is not None:
+                    print(pdf_loader.get_page_data(self._args.page).to_csv(index=False))
+                else:
+                    print(pdf_loader.get_data().to_csv(index=False))
+            else:
+                if self._args.page is not None:
+                    pdf_loader.save_page_data(self._args.page, filename)
+                else:
+                    pdf_loader.save_data(filename)
 
     def __save_text(self, text:str, filename: str):
         try:
@@ -102,7 +127,7 @@ class CLIController():
                 f.write(text)
         except FileNotFoundError as e:
             self._logger.error(e)
-            print("Output directory not found")
+            raise CLIException("Output directory not found")
 
 if __name__ == "__main__":
     try:
