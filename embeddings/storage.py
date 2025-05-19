c3d7e1bbfff4f1253a70ef72649da8ea63fd1891
Roberto Garcia <betogarcia97@live.com.mx>
2025-05-19 11:40:07 -0600

    A new simple script was created to serve as example of how to query relevant documents

--- embeddings/storage.py
+++ embeddings/storage.py
@@ -1,12 +1,16 @@
-from typing import List
 from abc import ABC, abstractmethod
 import chromadb
+import chromadb.errors
 import pandas as pd
 
 class Storage(ABC):
 
     @abstractmethod
-    def save_info(self, name:str, sentences:List[str], metadatas:List[str], embeddings:List[str]=None):
+    def save_info(self, name:str, sentences:list[str], metadatas:list[str], embeddings:list[str]=None):
+        pass
+
+    @abstractmethod
+    def query_sentence(self, collection:str, sentence:str, n_results:int) -> list[dict]:
         pass
 
 class ChromaDBStorage(Storage):
@@ -14,7 +18,7 @@ class ChromaDBStorage(Storage):
     def __init__(self):
         self.client = chromadb.PersistentClient(path="./db")
 
-    def save_info(self, name:str, sentences:List[str], metadatas:List[str], embeddings:List[str]=None):
+    def save_info(self, name:str, sentences:list[str], metadatas:list[str], embeddings:list[str]=None):
         try:
             collection = self.client.get_collection(name)
         except chromadb.errors.NotFoundError as e:
@@ -26,9 +30,30 @@ class ChromaDBStorage(Storage):
             ids = [str(i+1) for i in range(len(sentences))]
         )
 
+    def query_sentence(self, collection, sentence, n_results):
+        try:
+            collection = self.client.get_collection(collection)
+        except chromadb.errors.NotFoundError as e:
+            return None
+
+        results = collection.query(
+            query_texts=[sentence],
+            n_results=n_results
+        )
+
+        documents = []
+        for i in range(len(results['ids'][0])):
+            doc = {
+                'content': results['documents'][0][i],
+                'metadata': results['metadatas'][0][i],
+            }
+            documents.append(doc)
+
+        return documents
+
 class CSVStorage(Storage):
 
-    def save_info(self, name:str, sentences:List[str], metadatas:List[str], embeddings:List[str]=None):
+    def save_info(self, name:str, sentences:list[str], metadatas:list[str], embeddings:list[str]=None):
         df_dict = {
             'sentences': sentences,
             'metadatas': metadatas,
@@ -42,3 +67,6 @@ class CSVStorage(Storage):
         df = pd.DataFrame(df_dict)
 
         df.to_csv(name, sep=',', index=False)
+
+    def query_sentence(self, collection:str, sentence:str, n_results:int) -> list[dict]:
+        return super().query_sentence(collection, sentence, n_results)
